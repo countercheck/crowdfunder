9ba9f3c9edcabc81eb75e6e2bac337b2ae2b53c0
Michael Atlin <mike.atlin@gmail.com>
2014-02-25 14:04:53 -0500

    create campaign_params, generated schema

--- crowdfunder/app/controllers/campaigns_controller.rb
+++ crowdfunder/app/controllers/campaigns_controller.rb
@@ -1,2 +1,36 @@
 class CampaignsController < ApplicationController
+
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def campaign_params
+    params.require(:campaign).permit(:title, :description, :target_in_cents, :start_date, :end_date)
+  end
+
 end
