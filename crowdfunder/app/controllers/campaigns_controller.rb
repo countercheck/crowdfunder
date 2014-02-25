6b61573463ae74100b2c4710c8be19eb3394d2ce
Michael Atlin <mike.atlin@gmail.com>
2014-02-25 15:05:30 -0500

    generated campaign actions and routes

--- crowdfunder/app/controllers/campaigns_controller.rb
+++ crowdfunder/app/controllers/campaigns_controller.rb
@@ -5,27 +5,39 @@ class CampaignsController < ApplicationController
   end
 
   def new
-    
+  @campaign = Campaign.new    
   end
 
   def create
-    
+    @campaign = Campaign.new(campaign_params)
+    if @campaign.save
+      redirect_to campaign_path
+    else
+      render :new
+    end
   end
 
   def show
-    
+    @campaign = Campaign.find(params[:id])
   end
 
   def edit
-    
+    @campaign = Campaign.find(params[:id])
   end
 
   def update
-    
+    @campaign = Campaign.find(params[:id])
+    if @campaign.update_attributes(campaign_params)
+      redirect_to campaign_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-    
+    @campaign = Campaign.find(params[:id])
+    @product.destroy
+    redirect to campaigns_path
   end
 
   private
--- crowdfunder/config/routes.rb
+++ crowdfunder/config/routes.rb
@@ -1,4 +1,6 @@
 Crowdfunder::Application.routes.draw do
+
+  resources :campaigns
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
