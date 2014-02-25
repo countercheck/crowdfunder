230b269114dfbdc42c00684ca286bc6b01bab9b1
arovillard <arovillard@gmail.com>
2014-02-25 13:17:18 -0500

    Rails app generated with postgresql

---
+++ crowdfunder/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdfunder::Application.config.secret_key_base = '18555440eac0e84e3ec675d1629ece89f3de865fe7dd99a9b4e7385a356c607c7a5e2e4c57d0bb8a9c1297fb78439ad851eb875d72226736aa139d4db66cc98f'
